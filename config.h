1e7a27365b0f2a89d150d59409ee79941e645afe
David <rynstheoverlord@gmail.com>
2026-04-07 16:17:55 +0100

    defined human friendly names for the modifiers and created defaults

--- config.h
+++ config.h
@@ -1,6 +1,11 @@
 #ifndef MEADOW_CONFIG_H
 #define MEADOW_CONFIG_H
 
+#include "utils.h"
+
+#define MODIFIER SUPER_KEY
+#define WM_EXIT_KEY "q"
+
 #define APP_LAUNCHER "dmenu_run"
 
 #endif // !MEADOW_CONFIG_H
--- utils.h
+++ utils.h
@@ -4,6 +4,10 @@
 #include "meadow.h"
 #include <X11/X.h>
 
+#define ALT_KEY Mod1Mask
+#define SUPER_KEY Mod4Mask
+#define SHIFT_KEY ShiftMask
+
 void grab_key_with_string(wm_t *wm, const char *k, unsigned int mod);
 KeyCode gen_keycode_from_string(wm_t *wm, const char *k, unsigned int mod);
 
